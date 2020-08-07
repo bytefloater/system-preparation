d00239a9dcaa0cfd258f91aa32baf14d88147770
bytefloater <67054721+bytefloater@users.noreply.github.com>
2020-08-07 01:53:08 +0100

    Update minimal.sh

--- minimal.sh
+++ minimal.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
+wait
 brew install mas
 brew install ffmpeg
 
